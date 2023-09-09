69b6899622ecc5b898b2128e42c573cbd99d7de9
bandvov <bandvov@gmail.com>
2023-09-09 21:49:46 +0300

    add configs

---
+++ src/configs/urls.js
@@ -0,0 +1,39 @@
+export const URLS = [
+  {
+    name: "About us",
+    url: "/about-us",
+  },
+  {
+    name: "Delivery and payment",
+    url: "/delivary-and-payment",
+  },
+  {
+    name: "Faq",
+    url: "/faq",
+  },
+  {
+    name: "Feedback",
+    url: "/feedback",
+  },
+  {
+    name: "Blog",
+    url: "/blog",
+  },
+  {
+    name: "Contacts",
+    url: "/contacts",
+  },
+  {
+    name: "More",
+    children: [
+      {
+        name: "Bonus",
+        url: "/bonus",
+      },
+      {
+        name: "For business",
+        url: "/for-business",
+      },
+    ],
+  },
+];
